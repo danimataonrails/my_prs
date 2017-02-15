1fcc61b8f1abf3773ad0a63c7f0b35df6bba5500
danimata <danimata@aspgems.com>
2017-02-15 01:49:28 +0100

    Show and destroy exercise type

--- app/controllers/ex_types_controller.rb
+++ app/controllers/ex_types_controller.rb
@@ -17,10 +17,10 @@ class ExTypesController < ApplicationController
   end
   
   def create
-    authorize :ex_type, :create?
     @ex_type = ExType.new(ex_type_params)
+    authorize @ex_type, :create?
     if @ex_type.save
-      flash[:notice]
+      flash[:notice] = ''
       redirect_to ex_types_path
     else
       flash[:errors] = @ex_type.errors.full_messages
@@ -28,10 +28,24 @@ class ExTypesController < ApplicationController
     end
   end
   
+  def destroy
+    authorize @ex_type, :show?
+  end
+  
+  def destroy
+    authorize @ex_type, :destroy?
+    if @ex_type.destroy
+      flash[:notice] = ''
+    else
+      flash[:errors] = ''
+    end
+    redirect_to ex_types_path
+  end
+  
   private
   
   def load_ex_type
-    @ex_type = params.has_key?(:id) ? policy(:ex_type).scope.find(params[:id]) : nil
+    @ex_type = params.has_key?(:id) ? policy_scope(ExType).find(params[:id]) : nil
   end
 
   def load_ex_types
@@ -39,6 +53,6 @@ class ExTypesController < ApplicationController
   end
 
   def ex_type_params
-    params.require(:ex_type).permit(policy(@ex_type).permitted_attributes)
+    params.require(:ex_type).permit(policy(:ex_type).permitted_attributes)
   end
 end
--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -20,24 +20,29 @@ module LayoutHelper
     end
   end
   
-  def table_head_for(scope, columns)
-    content_tag :tr do
-      columns.collect do |column|
-        content_tag :th, t(column, scope: scope)
-      end.join.html_safe
-    end.html_safe
+  def table_head_for(scope, columns, actions = true)
+    ths = columns.collect do |column|
+      content_tag :th, t(column, scope: scope)
+    end.join.html_safe
+    ths += content_tag(:th, '&nbsp;'.html_safe) if actions
+    content_tag(:tr, ths).html_safe
   end
   
-  def table_rows_for(data, columns)
+  def table_rows_for(data, columns, actions = true)
     html = []
     if data.any?
       data.collect do |datum|
-        row = content_tag :tr do
-          columns.collect do |column|
-            content_tag :td, datum.send(column)
+        tds = columns.collect do |column|
+          content_tag :td, datum.send(column)
+        end.join.html_safe
+        if actions
+          tds += content_tag :td do
+            [link_to(t('actions.show'), datum),
+              link_to(t('actions.edit'), [:edit, datum]),
+              link_to(t('actions.destroy'), datum, method: :delete)].join('&nbsp').html_safe
           end
         end
-        html << row
+        html << content_tag(:tr, tds)
       end
     else
       html << content_tag(:tr, content_tag(:td, t('.no_data'), colspan: columns.size))
@@ -45,6 +50,23 @@ module LayoutHelper
     html.join.html_safe
   end
   
+  def fields_for(object, columns, scope)
+    content_tag :dl do
+      columns.collect do |column|
+        content_tag(:dt, t(column, scope: scope)) +
+        content_tag(:dd, object.send(column))
+      end.join.html_safe
+    end
+  end
+
   def flash_messages
+  end
+  
+  def cancel_link
+    link_to(t('actions.cancel'), :back)
+  end
+
+  def submit_or_cancel
+    [cancel_link, button_tag(t('actions.submit'))].join('&nbsp;').html_safe
   end
 end
